9e9648efcdad8c45284de716e8f70c494f5d69be
HR <ireh.hr@gmail.com>
2016-08-26 18:38:55 +0430

    Initial commit

---
+++ src/main/java/ir/hri/bl/Square.java
@@ -0,0 +1,13 @@
+package ir.hri.bl;
+
+public class Square extends Shape {
+
+    public Square() {
+        type = "Square";
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Inside Square::draw() method.");
+    }
+}
---
+++ src/main/java/ir/hri/view/App.java
@@ -0,0 +1,19 @@
+package ir.hri.view;
+
+import ir.hri.bl.Shape;
+import ir.hri.bl.ShapeCache;
+
+public class App {
+    public static void main(String[] args) {
+        ShapeCache.loadCache();
+
+        Shape clonedShape = (Shape) ShapeCache.getShape("1");
+        System.out.println("Shape : " + clonedShape.getType());
+
+        Shape clonedShape2 = (Shape) ShapeCache.getShape("2");
+        System.out.println("Shape : " + clonedShape2.getType());
+
+        Shape clonedShape3 = (Shape) ShapeCache.getShape("3");
+        System.out.println("Shape : " + clonedShape3.getType());
+    }
+}
